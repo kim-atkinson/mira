0f6e63eba1e3b3315c9aac551254d5f09d056cbf
kim-atkinson <kimfromtoronto@gmail.com>
2023-05-22 21:57:02 -0400

    Added full page scroll animation. Up and down.

--- js/main.js
+++ js/main.js
@@ -9,3 +9,43 @@ function onLinkClick() {
   document.getElementsByTagName('features container').scrollIntoView();
   // will scroll to 4th h3 element
 }
+
+const animatedTags = document.querySelectorAll(" h1, h2, h3, p, grid, section, img, .message, .monthly-insights, .kickoff, a.btn, svg")
+
+// fade out on load
+animatedTags.forEach(tag => {
+  tag.style.opacity = 0;
+})
+
+const fadeIn = function () {
+  // look through all the animated tags and see
+  // withthe getBoundingClientRect if it's inthe window
+
+  let delay = 0.25
+  animatedTags.forEach(tag => {
+    const tagTop = tag.getBoundingClientRect().top
+    const tagBottom = tag.getBoundingClientRect().bottom 
+    
+    if (tagTop < window.innerHeight && tagBottom > 0) {
+      tag.style.animation = `fadein 1s ${delay}s both`
+      
+    } else {
+      tag.style.opacity = 0
+      tag.style.animation = ""
+    }
+  })
+
+}
+
+// on load, run fadeIn
+fadeIn(); 
+
+// on scroll, run fadeIn
+document.addEventListener("scroll", function() {
+  fadeIn()
+})
+
+// on browser resize, run fadeIn
+window.addEventListener("resize", function () {
+  fadeIn()
+})
